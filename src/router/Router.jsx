8e149f0f9a9b129dc92ae3ec41efa80a2e128eed
LJS0000 <leejisu2281@gmail.com>
2024-11-25 11:27:31 +0900

    Feat:  프로젝트 세팅

---
+++ src/router/Router.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// layout
+import MainLayout from '../components/layout/MainLayout';
+// pages
+import HomePage from '../pages/HomePage';
+import AuthPage from '../pages/AuthPage';
+import DetailPage from '../pages/DetailPage';
+import NotFoundPage from '../pages/NotFoundPage';
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter
+      future={{ v7_relativeSplatPath: true, v7_startTransition: true }}
+    >
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/login" element={<AuthPage />} />
+          <Route path="/signup" element={<AuthPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
