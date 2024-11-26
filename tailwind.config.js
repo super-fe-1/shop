f9e73e92c067719ae5c9ba7a1945b805c5b8c4c0
LJS0000 <leejisu2281@gmail.com>
2024-11-26 22:23:31 +0900

    UI: 디자인 토큰 테일윈드 변수로 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#ffffff', // 기본 배경색
+        'background-secondary': '#f5f5f5', // 섹션 배경색
+        primary: '#302e81', // 주제색
+        'text-primary': '#333333', // 기본 텍스트 색상
+        'text-secondary': '#555555', // 보조 텍스트 색상
+        accent: '#9897c0', // 강조색
+        'accent-hover': '#6463a1', // 강조색 호버 상태
+        success: '#3e6f69', // 성공 상태 색상
+        error: '#cf383f', // 에러 상태 색상
+      },
+      fontFamily: {
+        pretendard: ['Pretendard', 'sans-serif'], // Pretendard 폰트
+      },
+    },
   },
   plugins: [],
 };
