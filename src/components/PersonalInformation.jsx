0c030afc6671854c21e8b80d91486140326131c7
Sunyoung Lee <s3u0n4@gmail.com>
2024-11-29 19:39:08 +0900

    Feat: order 페이지 css 이름 수정

--- src/components/PersonalInformation.jsx
+++ src/components/PersonalInformation.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from "../styles/components/PersonalInformation.css";
+import styles from "../styles/components/PersonalInformation.module.css";
 import {Link} from "react-router-dom";
 
 const PersonalInformation = () => {
