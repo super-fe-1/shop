8811ef2199c6e5daefddb1b5a910ea567585fb41
LJS0000 <leejisu2281@gmail.com>
2024-12-04 13:16:19 +0900

    Fix: store 문법 에러 수정

--- src/redux/store/configStore.js
+++ src/redux/store/configStore.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { userSlice } from '../modules/user';
+import userSlice from '../modules/user';
 
 const store = configureStore({
   reducer: { user: userSlice },
