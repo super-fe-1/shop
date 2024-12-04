904c3b6b5a507b1c08bf84bdce37d3b25e99f816
Sunyoung Lee <s3u0n4@gmail.com>
2024-12-04 21:37:18 +0900

    Feat: ShopList api 연결

--- src/components/ShopList.jsx
+++ src/components/ShopList.jsx
@@ -1,43 +1,53 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import styles from "../styles/components/ShopList.module.css"
+import axios from "../axios/axios";
+import {useEffect, useState} from "react";
+
 
 const ShopList = () => {
+  const [items, setItems] = useState([]);
 
-  return (
-    <>
-      <div className={styles.list__container}>
-        <p className={styles.list__title}>New Arrivals</p>
-        <p className={styles.list__description}>
-          Thoughtfully designed objects for the workspace, home, and travel
-        </p>
-      </div>
-      <div className={styles.list__filters}>
-        <ul className={styles.list__sort}>Sort</ul>
-        <ul className={styles.list__menu}>
-          <li>Category</li>
-          <li>Brand</li>
-          <li>Color</li>
-          <li>Sizes</li>
-        </ul>
-      </div>
-      <ul className={styles.list__items}>
-        {[1, 2, 3, 4, 5, 6].map((id) => (
-          //이미지 들어오면 구현 예정
-          <li key={id}>
-            <img src={`image${id}.jpg`} alt={`Image ${id}`} className="w-full h-auto" />
-            <div className={styles.list__buttons}>
-              <p
-                className={styles.list__link}
-              >
-                <Link to ="/detail/:id">이름 {id}</Link>
-              </p>
-              <p>가격</p>
-            </div>
-          </li>
-        ))}
+  useEffect(() => {
+    const getShopList = async () => {
+      const res = await axios.get(`/productDtoes`);
+      //const res = await axios.get(`api/products`);
+
+      setItems(res.data);
+    }
+    getShopList();
+  }, []);
+
+  return (<>
+    <div className={styles.list__container}>
+      <p className={styles.list__title}>New Arrivals</p>
+      <p className={styles.list__description}>
+        Thoughtfully designed objects for the workspace, home, and travel
+      </p>
+    </div>
+    <div className={styles.list__filters}>
+      <ul className={styles.list__sort}>Sort</ul>
+      <ul className={styles.list__menu}>
+        <li>Category</li>
+        <li>Brand</li>
+        <li>Color</li>
+        <li>Sizes</li>
       </ul>
-    </>
-  );
+    </div>
+    <ul className={styles.list__items}>
+      {items.map((item) => (
+        <li key={item.productId}>
+          <img src={item.productImageUrl} alt={`Image of ${item.title}`}/>
+          <div className={styles.list__buttons}>
+            <p
+              className={styles.list__link}
+            >
+              <Link to={`/detail/${item.productId}`}>{item.title}</Link>
+            </p>
+            <p>{item.price}</p>
+          </div>
+        </li>))}
+    </ul>
+  </>);
 };
 
 export default ShopList;
