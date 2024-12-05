bd5af0838e3dcf443d531c7c54a503a555e97b49
LJS0000 <leejisu2281@gmail.com>
2024-12-05 20:05:57 +0900

    Fix: children > Outlet 으로 수정

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,7 +1,8 @@
+import { Outlet } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = ({ isLog, children }) => {
-  return isLog ? <>{children}</> : <Navigate to="/login" replace />;
+const PrivateRoute = ({ isLog }) => {
+  return isLog ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
 export default PrivateRoute;
